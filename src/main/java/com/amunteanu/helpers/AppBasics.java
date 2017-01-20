e054583353756d412415592939d638de17616878
amunteanu9 <maxcool.alex@gmail.com>
2017-01-19 18:16:07 -0800

    Add requestbooleanFromUser method

--- src/main/java/com/amunteanu/helpers/AppBasics.java
+++ src/main/java/com/amunteanu/helpers/AppBasics.java
@@ -34,10 +34,37 @@ public class AppBasics {
 		return scanner.nextLine();
 	}
 
+	public static boolean requestbooleanFromUser(String question) {
+		String response;
+		boolean answerReceived = false;
+		boolean answer = true;
+		while (!answerReceived) {
+			System.out.print(question + " ");
+			response = scanner.nextLine();
+			if (response.equalsIgnoreCase("yes")) {
+				answerReceived = true;
+				answer = true;
+			} else if (response.equalsIgnoreCase("no")) {
+				answerReceived = true;
+				answer = false;
+			}
+		}
+		return answer;
+	}
+
 	public static String requestInfoFromUser(String question) {
 		String response;
 		System.out.print(question + " ");
 		response = scanner.nextLine();
 		return response;
+	}
+
+	public static int requestIntFromUser(String question) {
+		String input;
+		int result;
+		System.out.print(question + " ");
+		input = scanner.nextLine();
+		result = Integer.parseInt(input);
+		return result;
 	}
 }
