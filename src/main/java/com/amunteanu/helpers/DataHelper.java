9535b1835470baa2a6456c1e9d81a77476b68986
amunteanu9 <maxcool.alex@gmail.com>
2017-02-09 21:46:54 -0800

    Modify DataHelper class and pom file, Add InvalidExelExtensionException class

--- src/main/java/com/amunteanu/helpers/DataHelper.java
+++ src/main/java/com/amunteanu/helpers/DataHelper.java
@@ -7,8 +7,13 @@
  */
 package com.amunteanu.helpers;
 
+import java.io.*;
 import java.sql.*;
 import java.util.*;
+
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
 
 /**
  * DataHelper //ADDD (description of class)
@@ -57,7 +62,10 @@ public class DataHelper {
 			throws ClassNotFoundException, SQLException {
 		Object[][] myData;
 		ArrayList<Object> myArrayData = new ArrayList<Object>();
+		// this throws an exception when you do not have the proper dependency
+		// in your pom
 		Class.forName(driverClassString);
+		// Create connection to database
 		Connection dbconn = DriverManager.getConnection(databaseStringUrl, username, password);
 		Statement stmt = dbconn.createStatement();
 		ResultSet rs = stmt.executeQuery("select * from " + tableName);
@@ -81,5 +89,116 @@ public class DataHelper {
 		stmt.close();
 		dbconn.close();
 		return myData;
+	}
+
+	public static Object[][] getExcelFileData(String fileLocation, String fileName, Boolean hasLabels)
+			throws InvalidExcelExtensionException {
+		Object[][] resultsObject;
+		String[] fileNameParts = fileName.split("[.]");
+		String extension = fileNameParts[fileNameParts.length - 1];
+		ArrayList<Object> results = null;
+		if (extension.equalsIgnoreCase("xlsx")) {
+			results = getNewExcelFileResults(fileLocation, fileName, hasLabels);
+		} else if (extension.equalsIgnoreCase("xls")) {
+			results = getOldExcelFileResults(fileLocation, fileName, hasLabels);
+		} else {
+			throw new InvalidExcelExtensionException();
+		}
+		resultsObject = new Object[results.size()][];
+		results.toArray(resultsObject);
+		return resultsObject;
+	}
+
+	@SuppressWarnings("deprecation")
+	private static ArrayList<Object> getNewExcelFileResults(String fileLocation, String fileName, Boolean hasLabels) {
+		ArrayList<Object> results = new ArrayList<Object>();
+		try {
+			String fullFilePath = fileLocation + fileName;
+			InputStream newExcelFormatFile = new FileInputStream(new File(fullFilePath));
+			XSSFWorkbook workbook = new XSSFWorkbook(newExcelFormatFile);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> rowIterator = sheet.iterator();
+			if (hasLabels) {
+				rowIterator.next();
+			}
+			while (rowIterator.hasNext()) {
+				ArrayList<Object> rowData = new ArrayList<Object>();
+				Row row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					switch (cell.getCellType()) {
+					case Cell.CELL_TYPE_BOOLEAN:
+						System.out.print(cell.getBooleanCellValue() + "\t\t\t");
+						rowData.add(cell.getBooleanCellValue());
+						break;
+					case Cell.CELL_TYPE_NUMERIC:
+						System.out.print(cell.getNumericCellValue() + "\t\t\t");
+						rowData.add(cell.getNumericCellValue());
+						break;
+					case Cell.CELL_TYPE_STRING:
+						System.out.print(cell.getStringCellValue() + "\t\t\t");
+						rowData.add(cell.getStringCellValue());
+						break;
+					}
+				}
+				Object[] rowDataObject = new Object[rowData.size()];
+				rowData.toArray(rowDataObject);
+				results.add(rowDataObject);
+				System.out.println("");
+			}
+			newExcelFormatFile.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return results;
+	}
+
+	private static ArrayList<Object> getOldExcelFileResults(String fileLocation, String fileName, Boolean hasLabels) {
+		ArrayList<Object> results = new ArrayList<Object>();
+		try {
+			String fullFilePath = fileLocation + fileName;
+			InputStream newExcelFormatFile = new FileInputStream(new File(fullFilePath));
+			HSSFWorkbook workbook = new HSSFWorkbook(newExcelFormatFile);
+			HSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> rowIterator = sheet.iterator();
+			if (hasLabels) {
+				rowIterator.next();
+			}
+			while (rowIterator.hasNext()) {
+				ArrayList<Object> rowData = new ArrayList<Object>();
+				Row row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					switch (cell.getCellType()) {
+					case Cell.CELL_TYPE_BOOLEAN:
+						System.out.print(cell.getBooleanCellValue() + "\t\t\t");
+						rowData.add(cell.getBooleanCellValue());
+						break;
+					case Cell.CELL_TYPE_NUMERIC:
+						System.out.print(cell.getNumericCellValue() + "\t\t\t");
+						rowData.add(cell.getNumericCellValue());
+						break;
+					case Cell.CELL_TYPE_STRING:
+						System.out.print(cell.getStringCellValue() + "\t\t\t");
+						rowData.add(cell.getStringCellValue());
+						break;
+					}
+				}
+				Object[] rowDataObject = new Object[rowData.size()];
+				rowData.toArray(rowDataObject);
+				results.add(rowDataObject);
+				System.out.println("");
+			}
+			newExcelFormatFile.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return results;
 	}
 }
---
+++ src/main/java/com/amunteanu/helpers/InvalidExcelExtensionException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: InvalidExcelExtensionException.java<br>
+ *
+ *   Munteanu, Alex<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 9, 2017
+ *   
+ */
+
+package com.amunteanu.helpers;
+
+
+/**
+ * InvalidExcelExtensionException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Munteanu, Alex
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class InvalidExcelExtensionException extends Exception {
+}
