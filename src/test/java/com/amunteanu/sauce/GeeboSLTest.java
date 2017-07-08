42e0e3ec159c7dabf3ccdffd7c9364a86ed62cef
amunteanu9 <maxcool.alex@gmail.com>
2017-07-07 17:46:07 -0700

    Modify GeeboSLTest

--- src/test/java/com/amunteanu/sauce/GeeboSLTest.java
+++ src/test/java/com/amunteanu/sauce/GeeboSLTest.java
@@ -1,77 +1,89 @@
 package com.amunteanu.sauce;
 
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.*;
-import org.testng.*;
-import org.testng.annotations.*;
+import com.amunteanu.helpers.AutoBasics;
+import com.amunteanu.sauce.exceptions.BrowserNotSupportedBySauceLabsException;
 
-import com.amunteanu.helpers.*;
-import com.amunteanu.sauce.exceptions.*;
-
-public class GeeboSLTest extends SauceLabsTest {
+public class GeeboSLTest extends SauceLabsTest
+{
 
 	/**
 	 * @param baseURL
 	 */
-	public GeeboSLTest() {
+	public GeeboSLTest()
+	{
 		super("https://geebo.com");
 	}
 
 	@Override
 	@BeforeMethod(groups = "firefox")
-	public void setupFirefox() {
+	public void setupFirefox()
+	{
 	}
 
 	@Override
 	@BeforeMethod(groups = "chrome", enabled = false)
-	public void setupChrome() {
+	public void setupChrome()
+	{
 	}
 
 	@Override
 	@BeforeMethod(groups = "ie", enabled = false)
-	public void setupIE() {
+	public void setupIE()
+	{
 	}
 
-	//@Test(dataProvider = "dataProvider", groups = "search-item")
+	// @Test(dataProvider = "dataProvider", groups = "search-item")
 	public void geeboSearch(String username, String accessKey, Browser browser, String version, String platform,
-			String itemName, String category, int count) throws BrowserNotSupportedBySauceLabsException {
+			String itemName, String category, int count) throws BrowserNotSupportedBySauceLabsException
+	{
 		preTest(username, accessKey, browser, version, platform);
 		WebElement searchField = this.getDriver().findElement(By.id("header_search"));
 		WebElement cat = this.getDriver().findElement(By.id("header_search_controller"));
 		Select catSelect = new Select(cat);
 		searchField.sendKeys(itemName);
 		catSelect.selectByVisibleText(category);
-		WebElement searchButton =
-				this.getDriver().findElement(By.xpath("html/body/table/tbody/tr[2]/td[2]/div[1]/div[3]/span/img"));
-		searchButton.click();
-		String expectedTitle = "Free Classifieds Ads: " + category + " at Geebo";
-		AutoBasics.takeScreenshot(this.getDriver(), "geebo");
-		Assert.assertEquals(this.getDriver().getTitle(), expectedTitle);
-	}
-	
-	@Test(dataProvider = "dataProvider")
-	public void geeboSearchWithWait(String username, String accessKey, Browser browser, String version, String platform,
-			String itemName, String category, int count) throws BrowserNotSupportedBySauceLabsException {
-		preTest(username, accessKey, browser, version, platform);
-		//WebElement searchField = this.getDriver().findElement(By.id("header_search"));
-		WebElement searchField = (new WebDriverWait(getDriver(), 10))
-			    .until(ExpectedConditions.presenceOfElementLocated(By.id("header_search")));
-		WebElement cat = this.getDriver().findElement(By.id("header_search_controller"));
-		Select catSelect = new Select(cat);
-		searchField.sendKeys(itemName);
-		catSelect.selectByVisibleText(category);
-		WebElement searchButton =
-				this.getDriver().findElement(By.xpath("html/body/table/tbody/tr[2]/td[2]/div[1]/div[3]/span/img"));
+		WebElement searchButton = this.getDriver()
+				.findElement(By.xpath("html/body/table/tbody/tr[2]/td[2]/div[1]/div[3]/span/img"));
 		searchButton.click();
 		String expectedTitle = "Free Classifieds Ads: " + category + " at Geebo";
 		AutoBasics.takeScreenshot(this.getDriver(), "geebo");
 		Assert.assertEquals(this.getDriver().getTitle(), expectedTitle);
 	}
 
-	//@Test(dependsOnGroups = "search-item", dataProvider = "dataProvider")
-	public void geeboVerifyNrOfResults(String itemName, String category, int count) {
+	@Test(dataProvider = "dataProvider")
+	public void geeboSearchWithWait(String username, String accessKey, Browser browser, String version, String platform,
+			String itemName, String category, int count) throws BrowserNotSupportedBySauceLabsException
+	{
+		preTest(username, accessKey, browser, version, platform);
+		// WebElement searchField =
+		// this.getDriver().findElement(By.id("header_search"));
+		WebElement searchField = (new WebDriverWait(getDriver(), 10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.id("header_search")));
+		WebElement cat = this.getDriver().findElement(By.id("header_search_controller"));
+		Select catSelect = new Select(cat);
+		searchField.sendKeys(itemName);
+		catSelect.selectByVisibleText(category);
+		WebElement searchButton = this.getDriver()
+				.findElement(By.xpath("html/body/table/tbody/tr[2]/td[2]/div[1]/div[3]/span/img"));
+		searchButton.click();
+		String expectedTitle = "Free Classifieds Ads: " + category + " at Geebo";
+		AutoBasics.takeScreenshot(this.getDriver(), "geebo");
+		Assert.assertEquals(this.getDriver().getTitle(), expectedTitle);
+	}
+
+	// @Test(dependsOnGroups = "search-item", dataProvider = "dataProvider")
+	public void geeboVerifyNrOfResults(String itemName, String category, int count)
+	{
 		String[] text = this.getDriver().findElement(By.className("results_num")).getText().split(" ");
 		int numOfResults = Integer.parseInt(text[text.length - 1]);
 		this.getDriver().quit();
@@ -81,7 +93,8 @@ public class GeeboSLTest extends SauceLabsTest {
 
 	@Override
 	@DataProvider
-	public Object[][] dp() {
+	public Object[][] dp()
+	{
 		return new Object[][] { new Object[] { "toys", "Merchandise", 52 },
 				new Object[] { "QA Engineer software sf", "Merchandise", 107 },
 				new Object[] { "QA Engineer automation sf", "Merchandise", 74 } };
